6167a50dd6171eb6456b58ae323964381ef18380
sivals8 <sivals8@mcmaster.ca>
2025-09-06 01:35:37 -0400

    Updating admin auth and token

--- config/admin.js
+++ config/admin.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('STRAPI_ADMIN_JWT_SECRET'),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('STRAPI_API_TOKEN_SALT'),
   },
   transfer: {
     token: {
