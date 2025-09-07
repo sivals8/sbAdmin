3d8d54a03bd1d3d11c4f82f42af66c351f8d8fd2
sivals8 <sivals8@mcmaster.ca>
2025-09-07 12:43:01 -0400

    Updating cloudinary data

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,24 @@
-module.exports = () => ({});
+  
+  // ./config/plugins.js
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),        // aka API Key
+        api_secret: env('CLOUDINARY_SECRET'),  // aka API Secret
+      },
+      actionOptions: {
+        upload: {
+          // optional niceties:
+          folder: env('CLOUDINARY_FOLDER', 'strapi'),
+          // If you plan to upload videos too:
+          // resource_type: 'auto'
+        },
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
+});
