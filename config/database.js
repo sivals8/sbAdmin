d0d342280bd3cc847a767229f7ada6a396684ac7
sivals8 <sivals8@mcmaster.ca>
2025-09-10 16:39:16 -0400

    default to postgres

--- config/database.js
+++ config/database.js
@@ -1,12 +1,18 @@
 module.exports = ({ env }) => ({
   connection: {
-    client: 'postgres',
-    connection: {
-      connectionString: env('DATABASE_URL'),
-      ssl: {
-        rejectUnauthorized: false, // required for Supabase
-      },
-    },
+    client: env('DATABASE_CLIENT', 'sqlite'),
+    connection:
+      env('DATABASE_CLIENT') === 'postgres'
+        ? {
+            connectionString: env('DATABASE_URL'),
+            ssl: {
+              rejectUnauthorized: false, // required for Render / Supabase
+            },
+          }
+        : {
+            filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+          },
+    useNullAsDefault: env('DATABASE_CLIENT') !== 'postgres',
     pool: {
       min: 0,
       max: 10,
