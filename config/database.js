70457590c9db9cb22673ba9c68545af9ff748eda
sivals8 <sivals8@mcmaster.ca>
2025-09-11 17:51:36 -0400

    update to database

--- config/database.js
+++ config/database.js
@@ -1,17 +1,16 @@
 module.exports = ({ env }) => ({
   connection: {
-    client: env('DATABASE_CLIENT', 'sqlite'),
-    connection:
-      env('DATABASE_CLIENT') === 'postgres'
-        ? {
-            connectionString: env('DATABASE_URL'), //pulls from environment
-            ssl: { rejectUnauthorized: false },
-          }
-        : {
-            filename: env('DATABASE_FILENAME', '.tmp/data.db'), //holds data locally if it defaults to sqlite
-          },
-    useNullAsDefault: env('DATABASE_CLIENT') !== 'postgres',
-    pool: { min: 0, max: 10 },
+    client: 'postgres',
+    connection: {
+      connectionString: env('DATABASE_URL'),
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
+    pool: {
+      min: 0,
+      max: 10,
+    },
     acquireConnectionTimeout: 60000,
   },
 });
