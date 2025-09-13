8f70f5116cf660ba4714ba88bdc26400c8bf1266
sivals8 <sivals8@mcmaster.ca>
2025-09-13 00:28:14 -0400

    updated database

--- config/admin.js
+++ config/admin.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('STRAPI_ADMIN_JWT_SECRET'),
+    secret: env('JWT_SECRET'),
   },
   apiToken: {
     salt: env('STRAPI_API_TOKEN_SALT'),
