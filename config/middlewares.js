5a74c2fe789d6d9978c826e04731725b61a6b62c
sivals8 <sivals8@mcmaster.ca>
2025-09-13 01:43:22 -0400

    removing cloudinary

--- config/middlewares.js
+++ config/middlewares.js
@@ -9,8 +9,8 @@ module.exports = [
         useDefaults: true,
         directives: {
           'connect-src': ["'self'", 'https:'],
-          'img-src': ["'self'", 'data:', 'blob:', 'market-assets.strapi.io', 'res.cloudinary.com'],
-          'media-src': ["'self'", 'data:', 'blob:', 'market-assets.strapi.io', 'res.cloudinary.com'],
+          'img-src': ["'self'", 'data:', 'blob:', 'market-assets.strapi.io', 'sbadmin-portal.onrender.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'market-assets.strapi.io', 'sbadmin-portal.onrender.com'],
           upgradeInsecureRequests: null,
         },
       },
