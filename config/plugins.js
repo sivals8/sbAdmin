f8904e3ee5c9fe850131dddfb06a4f80a2a873a9
sivals8 <sivals8@mcmaster.ca>
2025-09-13 01:18:15 -0400

    removing cloudinary

--- config/plugins.js
+++ config/plugins.js
@@ -3,21 +3,8 @@
 module.exports = ({ env }) => ({
   upload: {
     config: {
-      provider: 'cloudinary',
+      provider: 'local',
       providerOptions: {
-        cloud_name: env('CLOUDINARY_NAME'),
-        api_key: env('CLOUDINARY_KEY'),        // aka API Key
-        api_secret: env('CLOUDINARY_SECRET'),  // aka API Secret
-      },
-      actionOptions: {
-        upload: {
-          // optional niceties:
-          folder: env('CLOUDINARY_FOLDER', 'strapi'),
-          // If you plan to upload videos too:
-          // resource_type: 'auto'
-        },
-        uploadStream: {},
-        delete: {},
       },
     },
   },
