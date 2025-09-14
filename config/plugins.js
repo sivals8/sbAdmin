3efa2ca1154d4d0b6759432d230a4fa8fecf746e
sivals8 <sivals8@mcmaster.ca>
2025-09-14 19:51:22 -0400

    updating plugins

--- config/plugins.js
+++ config/plugins.js
@@ -1,10 +1,17 @@
-  
-  // ./config/plugins.js
+
 module.exports = ({ env }) => ({
   upload: {
     config: {
-      provider: 'local',
+      provider: 'cloudinary',
       providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),       
+        api_secret: env('CLOUDINARY_SECRET'),  
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
       },
     },
   },
